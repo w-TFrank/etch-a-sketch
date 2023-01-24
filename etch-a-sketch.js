a771bda3244c1e4dfa0a03b3fc50fcc882218a15
TFrank <madmonsterHI@gmail.com>
2023-01-24 18:16:31 -0500

    grid can now be any #x# and still make a square

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,9 +3,12 @@ for (let i = 1; i <= 256; i++){
     let square = document.createElement('div');
     let br = document.createElement('div');
     br.className = "break";
+    let squareWidth = 1000 / 16;
     if ((i % 16) === 1){
         grid.appendChild(br);
     }
     grid.appendChild(square);
-    square.className = "square";
+    square.className = 'square';
+    square.style.width = squareWidth.toString() + "px";
+    square.style.height = squareWidth.toString() + "px";
 }
