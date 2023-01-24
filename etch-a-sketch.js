98cfd313b5d9d2305c2f9d12e96ff655cee3e7ce
TFrank <madmonsterHI@gmail.com>
2023-01-24 18:51:52 -0500

    squares now change color when moused over

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,14 +1,29 @@
-for (let i = 1; i <= 256; i++){
-    let grid = document.querySelector('#grid');
-    let square = document.createElement('div');
-    let br = document.createElement('div');
-    br.className = "break";
-    let squareWidth = 1000 / 16;
-    if ((i % 16) === 1){
-        grid.appendChild(br);
+let square = document.createElement('div');
+square.className = 'square';
+square.id = 'square';
+function createGrid() {
+    for (let i = 1; i <= 256; i++){
+        let grid = document.querySelector('#grid');
+        let br = document.createElement('div');
+        square = document.createElement('div');
+        square.className = 'square';
+        square.id = 'square';
+        br.className = "break";
+        let squareWidth = 1000 / 16;
+        if ((i % 16) === 1){
+            grid.appendChild(br);
+        }
+        grid.appendChild(square);
+        square.style.width = squareWidth.toString() + "px";
+        square.style.height = squareWidth.toString() + "px";
     }
-    grid.appendChild(square);
-    square.className = 'square';
-    square.style.width = squareWidth.toString() + "px";
-    square.style.height = squareWidth.toString() + "px";
+}
+
+createGrid();
+
+let squares = document.getElementsByClassName('square');
+for (const square of squares) {
+    square.addEventListener('mouseover', function () {
+        square.style.background = 'black';
+    });
 }
