6a7cfecd9d433716b7ac5edb68f3aab94b4d4eae
TFrank <madmonsterHI@gmail.com>
2023-01-24 21:53:10 -0500

    added button to make grid anything from 1x1 to 100x100

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,16 +1,17 @@
+let button = document.querySelector('button');
 let square = document.createElement('div');
 square.className = 'square';
 square.id = 'square';
-function createGrid() {
-    for (let i = 1; i <= 256; i++){
+function createGrid(numberOfSquares) {
+    for (let i = 1; i <= (numberOfSquares * numberOfSquares); i++){
         let grid = document.querySelector('#grid');
         let br = document.createElement('div');
         square = document.createElement('div');
         square.className = 'square';
         square.id = 'square';
         br.className = "break";
-        let squareWidth = 1000 / 16;
-        if ((i % 16) === 1){
+        let squareWidth = 1000 / numberOfSquares;
+        if ((i % numberOfSquares) === 1){
             grid.appendChild(br);
         }
         grid.appendChild(square);
@@ -19,7 +20,7 @@ function createGrid() {
     }
 }
 
-createGrid();
+createGrid(16);
 
 let squares = document.getElementsByClassName('square');
 for (const square of squares) {
@@ -27,3 +28,18 @@ for (const square of squares) {
         square.style.background = 'black';
     });
 }
+
+button.addEventListener('click', function() {
+    let userChoice = prompt("How many squares wide would you like the grid to be?");
+    if (userChoice > 100) {
+        alert("Please enter a number less than 100");
+    } else {
+        let squaresToRemove = document.getElementsByClassName('square');
+        let i = squaresToRemove.length - 1;
+        while (0 < squaresToRemove.length){
+            squaresToRemove[i].remove();
+            i--;
+        }
+        createGrid(userChoice);
+    }
+});
